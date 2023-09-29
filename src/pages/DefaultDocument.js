b2e2be7106c9f3c58f59f4172f21af633cfdb1ac
kimheeseok <kim1140129@gmail.com>
2023-09-29 12:10:43 +0900

    feat: css className 변경

--- src/pages/DefaultDocument.js
+++ src/pages/DefaultDocument.js
@@ -3,7 +3,7 @@ import Component from "../template/component.js";
 export default class DefaultDocument extends Component {
   view() {
     const $container = document.createElement("div");
-    $container.className = "DefaultDocument";
+    $container.className = "default_document";
     $container.innerHTML = `
       <span>페이지를 선택해 주세요</span>
     `;
