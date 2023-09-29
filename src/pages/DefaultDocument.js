0672d7c21a2985ecdad25ba11708c46367dc77bc
kimheeseok <kim1140129@gmail.com>
2023-09-29 11:22:49 +0900

    feat: Default 페이지 추가

--- src/pages/DefaultDocument.js
+++ src/pages/DefaultDocument.js
@@ -1,11 +1,13 @@
-export default function DefaultDocument() {
-  const $container = document.createElement("div");
-  $container.className = "DefaultDocument";
+import Component from "../template/component.js";
 
-  this.render = () => {
+export default class DefaultDocument extends Component {
+  view() {
+    const $container = document.createElement("div");
+    $container.className = "DefaultDocument";
     $container.innerHTML = `
       <span>페이지를 선택해 주세요</span>
     `;
+
     return $container;
-  };
+  }
 }
