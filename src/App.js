675f56136ddd9a1636f930befdfd3cedfe318d48
kimheeseok <kim1140129@gmail.com>
2023-09-29 11:22:33 +0900

    feat: 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import DefaultDocument from "./pages/DefaultDocument.js";
 import DocumentPage from "./pages/DocumentPage.js";
-import { renderPage } from "./routes/index.js";
+import { initRouter, renderPage } from "./routes/index.js";
 import Navigation from "./components/Navigation/Navigation.js";
-import Test from "./pages/test.js";
 import { defaultDocument } from "./constants/index.js";
 import { getItem } from "./utils/storage.js";
 
 export default class App {
   constructor({ $target }) {
     this.$target = $target;
-
-    this.state = defaultDocument;
-    const result = getItem("document");
-    if (result) {
-      this.state = JSON.parse(result);
-    }
-
-    this.render();
+    initRouter(this.route.bind(this)); // history 변경 감지,
+    window.addEventListener("popstate", this.route.bind(this));
+    this.route();
   }
 
-  render() {
-    const $navigation = new Navigation({
+  route() {
+    const { pathname } = location;
+
+    new Navigation({
       $target: this.$target,
-      props: this.state,
     });
+
+    if (pathname === "/") {
+      new DefaultDocument({ $target: this.$target });
+    }
   }
 }
